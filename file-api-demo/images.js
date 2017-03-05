8079a85206b155fda8146fef221a89abad2feb6a
Marshall Seid <Marchness@MM-2.local>
2017-03-05 03:39:19 -0800

    Modified user interface w/options

--- file-api-demo/images.js
+++ file-api-demo/images.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 		var fileInput = document.getElementById('fileInput');
 		var fileDisplayArea = document.getElementById('fileDisplayArea');
-
+		var user_input = document.getElementById('user_value')
+		const stopwords = ["a", "or", "and", "the", "in", "of", "on", "with"];
 		fileInput.addEventListener('change', function(e) {
 			var file = fileInput.files[0];
 			var imageType = /image.*/;
@@ -14,15 +15,18 @@ $(document).ready(function() {
 
 					var img = new Image();
 					img.src = reader.result;
+					console.log(img.src);
+					// Send to server,
+					MicrosoftPost("{'url':'https://www.w3schools.com/css/trolltunga.jpg'}")
 					// 	console.log(img.src)
 
 					//var blob = b64toBlob(img.src, "image/jpeg");
 					// console.log(img.src)
-					MicrosoftPost(img.src);
+					//MicrosoftPost(img.src);
 					fileDisplayArea.appendChild(img);
 				}
 
-				reader.readAsBinaryString(file);	
+				reader.readAsDataURL(file);	
 			} else {
 				fileDisplayArea.innerHTML = "File not supported!";
 			}
@@ -39,47 +43,33 @@ $(document).ready(function() {
 		        url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + $.param(params),
 		        beforeSend: function(xhrObj){
 		            // Request headers
-		            xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+		            xhrObj.setRequestHeader("Content-Type","application/json");
 		            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", SUBSCRIPTION_KEY);
 		        },
 		        //cache: true,
 		        type: "POST",
 		        // Request body - Curr just static image
-		        data: "{input}",
+		        data: input,
 		        dataType: "json"
 		    })
 		    .done(function(data) {
 		        alert("success");
 		        console.log(data);
+		        var res = data["description"]["captions"][0]["text"].split(" ")
+		        var map = {}
+		        for (i = 0; i < res.length; i++) {
+		        	if (stopwords.indexOf(res[i]) < 0) {
+		        		map[res[i]] = user_input.value; 
+		        	}
+		        }
+		        console.log(map);
+		        // Pass Map into Next function 
+
 		    })
 		    .fail(function() {
 		        alert("error in posting to MS");
 		    });
 		};
-		function b64toBlob(b64Data, contentType, sliceSize) {	
-
-		  contentType = contentType || '';
-		  sliceSize = sliceSize || 512;
-
-		  var byteCharacters = atob(b64Data);
-		  var byteArrays = [];
-
-		  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-		    var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-		    var byteNumbers = new Array(slice.length);
-		    for (var i = 0; i < slice.length; i++) {
-		      byteNumbers[i] = slice.charCodeAt(i);
-		    }
-
-		    var byteArray = new Uint8Array(byteNumbers);
-
-		    byteArrays.push(byteArray);
-		  }
-		    
-		  var blob = new Blob(byteArrays, {type: contentType});
-		  return blob;
-		}
 });
 
 // function MicrosoftPost(img) {
